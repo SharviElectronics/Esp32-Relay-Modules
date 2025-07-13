50399e8af3afe5205adb721acdf96d67abaff2e3
Rajkumar181 <suryadevkuswaha@gmail.com>
2025-07-13 18:32:27 +0530

    Update 4CH Relay TestCode.ino

--- 4CH Relay TestCode/4CH Relay TestCode.ino
+++ 4CH Relay TestCode/4CH Relay TestCode.ino
@@ -2,28 +2,23 @@
 #define RELAY2  25
 #define RELAY3  33
 #define RELAY4  32
-#define RelayArray[] = {26, 25, 33, 32}
-int i =0;
+
+int RelayArray[] = {RELAY1, RELAY2, RELAY3, RELAY4};
+int i = 0;
+
 void setup() {
   Serial.begin(115200);
 
-  pinMode(RELAY1, OUTPUT);
-  pinMode(RELAY2, OUTPUT);
-  pinMode(RELAY3, OUTPUT);
-  pinMode(RELAY4, OUTPUT);
-
-  // Turn all relays off initially
-  digitalWrite(RELAY1, LOW);
-  digitalWrite(RELAY2, LOW);
-  digitalWrite(RELAY3, LOW);
-  digitalWrite(RELAY4, LOW);
+  for (i = 0; i < 4; i++) {
+    pinMode(RelayArray[i], OUTPUT);
+    digitalWrite(RelayArray[i], LOW); // All relays off initially
+  }
 }
 
 void loop() {
-  for(i=0; i<4; i++)
-    {
-      digitalWrite(RelayArray[i], !digitalRead(RelayArray[i]));
-      delay(1000);
-    }
+  for (i = 0; i < 4; i++) {
+    digitalWrite(RelayArray[i], !digitalRead(RelayArray[i])); // Toggle state
+    delay(1000);
+  }
   delay(1000);
 }
