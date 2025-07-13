4da457f4794e07db9308b0fe31dc62a49fc19806
Rajkumar181 <suryadevkuswaha@gmail.com>
2025-07-13 17:15:22 +0530

    Create 4CH Relay TestCode.ino

---
+++ 4CH Relay TestCode/4CH Relay TestCode.ino
@@ -0,0 +1,28 @@
+#define RELAY1  26
+#define RELAY2  25
+#define RELAY3  33
+#define RELAY4  32
+#define RelayArray[] = {26, 25, 33, 32}
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(RELAY1, OUTPUT);
+  pinMode(RELAY2, OUTPUT);
+  pinMode(RELAY3, OUTPUT);
+  pinMode(RELAY4, OUTPUT);
+
+  // Turn all relays off initially
+  digitalWrite(RELAY1, LOW);
+  digitalWrite(RELAY2, LOW);
+  digitalWrite(RELAY3, LOW);
+  digitalWrite(RELAY4, LOW);
+}
+
+void loop() {
+  for(i=0; i<4; i++)
+    {
+      digitalWrite(RelayArray[i], !digitalRead(RelayArray[i]));
+      delay(1000);
+    }
+  delay(1000);
+}
